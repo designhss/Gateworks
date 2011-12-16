99226ae222e151730265d949d25a48096865b0ee
Paul Anderson <designhss@gmail.com>
2011-12-16 10:21:21 +0000

    Salt and security changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,16 +21,25 @@ user = find_by_email(email)
 return user if user && user.authenticated?(password)
 end
 def authenticated?(password)
-self.hashed_password == encrypt(password)
+self.hashed_password == encrypt(password + "ruby_rocks" + self.salt)
 end
 
   protected
 def encrypt_password
 return if password.blank?
-self.hashed_password = encrypt(password)
+salt = generate_salt
+self.hashed_password = encrypt(password + "ruby_rocks" + salt)
 end
 
   def encrypt(string)
 Digest::SHA1.hexdigest(string)
+
+def generate_salt
+  chars = ("a".."z").to_a + ("A".."Z").to_a + ("0".."9").to_a
+  salt = ""
+  1.upto(10) { |i| salt << chars[rand(chars.size-1)] }
+  self.salt = salt
 end
+  end
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111207125304) do
+ActiveRecord::Schema.define(:version => 20111215205607) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(:version => 20111207125304) do
     t.string   "hashed_password"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "salt"
   end
 
 end
